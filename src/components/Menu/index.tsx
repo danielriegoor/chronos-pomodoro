183c05dacde85eba4753fb1562c5f0c02768cd2c
Daniel Riêgo <danielriegodevjobs@gmail.com>
2025-12-19 19:08:10 -0300

    Páginas prontas

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -61,7 +61,7 @@ export function Menu() {
       </RouterLink>
       <RouterLink
         className={styles.menuLink}
-        href='#'
+        href='/settings/'
         aria-label='Configurações'
         title='Configurações'
       >
---
+++ src/pages/Settings/index.tsx
@@ -0,0 +1,50 @@
+// PascalCase para componentes React
+
+import { SaveIcon } from 'lucide-react';
+import { Container } from '../../components/Container';
+import { DefaultButton } from '../../components/DefaultButton';
+import { DefaultInput } from '../../components/DefaultInput';
+import { Heading } from '../../components/Heading';
+import { MainTemplate } from '../../templates/MainTemplate';
+
+export function Settings() {
+  return (
+    <MainTemplate>
+      <Container>
+        <Heading>Configurações</Heading>
+      </Container>
+      <Container>
+        <p style={{ textAlign: 'center' }}>
+          Modifique as configurações para tempo de foco, descanso curto e
+          descanso longo.
+        </p>
+      </Container>
+
+      <Container>
+        <form action='' className='form'>
+          <div className='formRow'>
+            <DefaultInput id='workTime' labelText='Foco' />
+          </div>
+          <div className='formRow'>
+            <DefaultInput id='shortBreakTime' labelText='Pausa curta' />
+          </div>
+          <div className='formRow'>
+            <DefaultInput
+              id='longBreakTime'
+              labelText='Pausa longa'
+              aria-label='Salvar configurações'
+              title='alvar configurações'
+            />
+          </div>
+          <div className='formRow'>
+            <DefaultButton
+              icon={<SaveIcon />}
+              aria-label='Salvar configurações'
+              title='Salvar configurações'
+            />
+          </div>
+        </form>
+      </Container>
+    </MainTemplate>
+  );
+}
